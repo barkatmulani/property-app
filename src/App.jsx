972567ca979399fd24c842e3c8e76f6c0f7259e3
DESKTOP-D7FN8BE\barkat <barkatmulani@yahoo.com>
2020-03-22 22:43:44 +1100

    Working.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from 'redux';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/main';
 
-class App {
-  render() {
-    return (
-      <Provider store={store}>
-        <Main />
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <Main />
+  );
 }
 
 export default App;
